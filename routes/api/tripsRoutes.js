228c2907c3f0636ccb8933bffb8300b3fe8bfed1
AKJ <kjajila@gmail.com>
2022-11-08 17:19:20 -0500

    commit after connect to heroku

--- routes/api/tripsRoutes.js
+++ routes/api/tripsRoutes.js
@@ -1,33 +1,33 @@
-const router = require('express').Router();
-const { Location, Traveller, Trips } = require('../../models');
+// const router = require('express').Router();
+// const { Location, Traveller, Trips } = require('../../models');
 
-router.get('/', async (req, res) => {
-  try {
-    const tripsData = await Trips.findAll({
-      include: [{ model: Traveller }],
-    });
-    res.status(200).json(tripsData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.get('/', async (req, res) => {
+//   try {
+//     const tripsData = await Trips.findAll({
+//       include: [{ model: Traveller }],
+//     });
+//     res.status(200).json(tripsData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-// GET a single traveller
-router.get('/:id', async (req, res) => {
-  try {
-    const tripsData = await Trips.findByPk(req.params.id, {
-      include: [{ model: Traveller }],
-    });
+// // GET a single traveller
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const tripsData = await Trips.findByPk(req.params.id, {
+//       include: [{ model: Traveller }],
+//     });
 
-    if (!tripsData) {
-      res.status(404).json({ message: 'No tripsData found with that id!' });
-      return;
-    }
+//     if (!tripsData) {
+//       res.status(404).json({ message: 'No tripsData found with that id!' });
+//       return;
+//     }
 
-    res.status(200).json(travellerData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.status(200).json(travellerData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
