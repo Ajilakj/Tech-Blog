cb9a356714f29f91f1637af2b4ae4ccc807c15c7
AKJ <kjajila@gmail.com>
2022-11-28 18:31:22 -0500

    commit after testing

--- controllers/home.js
+++ controllers/home.js
@@ -1,21 +1,37 @@
 const router = require('express').Router();
 const { User, Blog } = require('../models');
 
-router.get('/', async (req, res) => {
-  try {
-    const dbBlogsData = await Blog.findAll();
-    console.log(dbBlogsData);
-    const blogData = dbBlogsData.map((blogData) =>
-    blogData.get({ plain: true }));
-    res.render('homepage', {
-      blogData,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+// router.get('/', async (req, res) => {
+//   try {
+//     const dbBlogsData = await Blog.findAll();
+//     console.log(dbBlogsData);
+//     const blogData = dbBlogsData.map((blogData) =>
+//     blogData.get({ plain: true }));
+//     res.render('homepage', {
+//       blogData,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+// router.get("/:id", authMiddleware, async (req, res) => {
+router.get("/", async (req, res) => {
+  const dbBlogsData = await Blog.findAll();
+  //   {
+  //     order: [
+  //       ["date_created", "DESC"],
+  //     ]
+  //   }
+  // );
+  console.log(dbBlogsData);
+  const blogData = dbBlogsData.map((blog) =>
+  blog.get({ plain: true })
+  );
+  console.log(blogData);
 
+  res.render("homepage", {blogData});
+});
 
    router.get('/login', async (req, res) => {
     res.render('login-user');
